f1ce015cde02b3b5bf657860d82d99d1729bd43a
ShrutiPm <87891853+ShrutiPm@users.noreply.github.com>
2024-01-28 00:42:59 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  feature {}
+}
+
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
   location = var.location
@@ -35,7 +39,6 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
 
 terraform {
   backend "azurerm" {
-    features {}
     # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
     # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
     # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
