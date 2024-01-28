28ba189092e241a7bbc375a19d4d7bb833f048dd
ShrutiPm <87891853+ShrutiPm@users.noreply.github.com>
2024-01-28 14:46:39 +0530

    Update outputs.tf

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -1,9 +1,4 @@
 
-output "cluster_username" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
-}
-
-
 output "host" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
 }
